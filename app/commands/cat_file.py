4a9ca81c9412ed4eeff39220e9e4723cdce17c34
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-05-11 21:40:37 +0800

    codecrafters submit [skip ci]

---
+++ app/commands/cat_file.py
@@ -0,0 +1,11 @@
+import sys, zlib
+from .base import Command
+
+class CatFile(Command):
+    def execute(self, args):
+        blob_hash = sys.argv[3]
+        obj_path = f".git/objects/{blob_hash[:2]}/{blob_hash[2:]}"
+        with open(obj_path, "rb") as f:
+            decompressed = zlib.decompress(f.read())
+        null_idx = decompressed.index(b"\x00")
+        sys.stdout.buffer.write(decompressed[null_idx + 1:])
