60d415ec680a0a4924b1421bd87ccc1fa91847f7
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-05-11 21:39:24 +0800

    codecrafters submit [skip ci]

---
+++ app/commands/init.py
@@ -0,0 +1,10 @@
+import os
+from .base import Command
+
+class Init(Command):
+    os.mkdir(".git")
+    os.mkdir(".git/objects")
+    os.mkdir(".git/refs")
+    with open(".git/HEAD", "w") as f:
+        f.write("ref: refs/heads/main\n")
+    print("Initialized git directory")
