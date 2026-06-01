011f0ad3ede61c4fc62d634162f2179f52e94d87
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-06-02 00:41:19 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,13 +1,32 @@
-  import sys                                                                                                                                                                                                                                                                 
-  from app.commands import COMMANDS
+import sys
+import os
+import zlib
 
-  def main():
-      name, *args = sys.argv[1:]
-      try:
-          cmd_cls = COMMANDS[name]
-      except KeyError:                                                                                                                                                                                                                                                         
-          raise SystemExit(f"Unknown command: {name}")
-      cmd_cls().execute(args)               # mirrors  find_command(cmd).execute(...)                                                                                                                                                                                          
-                                                                                                                                                                                                                                                                               
-  if __name__ == "__main__":
-      main() 
+
+def main():
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
+    print("Logs from your program will appear here!", file=sys.stderr)
+
+    command = sys.argv[1]
+    if command == "init":
+        os.mkdir(".git")
+        os.mkdir(".git/objects")
+        os.mkdir(".git/refs")
+        with open(".git/HEAD", "w") as f:
+            f.write("ref: refs/heads/main\n")
+        print("Initialized git directory")
+    elif command == "cat-file":
+        blob_hash = sys.argv[3]
+        obj_path = f".git/objects/{blob_hash[:2]}/{blob_hash[2:]}"
+        with open(obj_path, "rb") as f:
+            decompressed = zlib.decompress(f.read())
+        null_idx = decompressed.index(b"\x00")
+        sys.stdout.buffer.write(decompressed[null_idx + 1:])
+    elif command == "hash-object":
+        print("hash-object")
+    else:
+        raise RuntimeError(f"Unknown command #{command}")
+
+
+if __name__ == "__main__":
+    main()
