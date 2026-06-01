375482e5c74c421700c8bfaab493ee72b95b9f06
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-06-02 00:36:47 +0800

    codecrafters submit [skip ci]

--- app/commands/cat_file.py
+++ app/commands/cat_file.py
@@ -4,6 +4,8 @@ from .base import Command
 class CatFile(Command):
     def execute(self, args):
         blob_hash = sys.argv[3]
+        print(blob_hash[:2])
+        print(blob_hahs[2:])
         obj_path = f".git/objects/{blob_hash[:2]}/{blob_hash[2:]}"
         with open(obj_path, "rb") as f:
             decompressed = zlib.decompress(f.read())
